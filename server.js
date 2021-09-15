a8cc19aae68cdc0b63c4dcb3292d4fcba65e078c
morenm14 <morenm14@gmatc.matc.edu>
2021-09-15 18:07:13 -0500

    Created User Registration API Endpoit

--- server.js
+++ server.js
@@ -7,10 +7,28 @@ const app = express();
 //connect database
 connectDatabase();
 
+//Configure Middleware
+app.use(express.json({extended: false}));
+
 //API endpoints
+/** 
+ * @route GET/
+ * @desc Test end point
+*/
+
 app.get("/", (req, res) => 
     res.send("http get request sent to root api endpoint")
 );
+
+/** 
+ * @route POST api/users 
+ * @desc Register User
+*/
+app.post("api/users", (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
 
 //connection listener
 app.listen(3000, () => console.log("express server running on port 3000"));
