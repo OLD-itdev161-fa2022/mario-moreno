f61cb6b4cb2dc6ce8ebdbef01d9e1ca644b235ab
morenm14 <morenm14@gmatc.matc.edu>
2021-10-06 19:32:22 -0500

    added comments to server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express from "express";
 import connectDatabase from "./config/db";
 import {check, validationResult} from "express-validator";
 import jwt from "jsonwebtoken";
